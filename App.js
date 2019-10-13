1b756f2c5b29e4566f0f64ee999cec6520ea8f6a
Everything-is-modular <saneja368@gmail.com>
2019-10-13 14:40:31 +0530

    update

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './Components/Header'
 import Riders from './Components/Riders'
 class App extends React.Component {
+  
   constructor() {
     super()
     this.state={
@@ -10,12 +11,14 @@ class App extends React.Component {
     }
   }
   componentDidMount(){
-    fetch("https://my-json-server.typicode.com/Everything-is-modular/Ola/db").then((resp) =>{
-      resp.json()
+    fetch("https://my-json-server.typicode.com/Everything-is-modular/Ola/db")
+    .then((resp) =>{
+      return resp.json()
     })
-    .then((rides)=>{
+    .then((response)=>{
+      console.log("this is rides",response)
         this.setState({
-          rides
+          rides:response.rides
         })
     })
   }
@@ -24,7 +27,7 @@ class App extends React.Component {
       <div className="wrapper">
         <Header />
         {
-          rides.map((ride,index) => {
+          this.state.rides.map((ride,index) => {
             return (
               <Riders data={ride} key={`${index}`} />
             );
--- Components/Riders.js
+++ Components/Riders.js
@@ -5,21 +5,21 @@ import cancelled from '../Assets/Cancelled.jpeg'
 import moment from 'moment'
 
 let Riders = (props)=>{
+    console.log(props.data)
     return(
        
         <div className='ride-wrapper'>
-            { console.log(this.props.data)}
 
             <img src={auto} className='auto' />
             <div className='ride-info'>
-                <div className="ride-date">{moment(this.props.data.date).format('LLLL')}</div>
+                <div className="ride-date">{moment(props.data.date).format('LLLL')}</div>
                 <div>{`Auto CRN: ${props.data.CRN}`}</div>
 
             </div>
             
             <div className="location-info">
-                    <p className="source">{this.props.data.source}</p>
-                    <p className="destination">{this.props.data.destination}</p>
+                    <p className="source">{props.data.source}</p>
+                    <p className="destination">{props.data.destination}</p>
             </div>
             
         </div>
